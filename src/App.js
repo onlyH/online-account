a542525edf84cacb722c96d55be987b6e306c802
onlyh <736087284@qq.com>
2019-10-17 13:09:05 +0800

    add AppContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import axios from 'axios'
 import Home from "./containers/Home";
-
+import Create from "./containers/Create";
+import {FlatterArr, parseToYearAndMonth, ID } from "./utility";
+import { AppContext } from './AppContext'
 function App() {
   return (
       <div className="App ">
